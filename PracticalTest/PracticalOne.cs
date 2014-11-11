c92be05557a8a1fe11f25b28d37a0fddf7797990
Mike Nelson <mnelson@surgeforward.com>
2014-11-11 10:20:13 -0700

    solutions

---
+++ PracticalTest/PracticalOne.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PracticalTest
+{
+    public class PracticalOne : IPracticalOne
+    {
+        public String ReverseWordsInString(String words)
+        {
+            if (string.IsNullOrEmpty(words))
+                return words;
+
+            var reversed = words.Split(' ').Aggregate("", (current, word) => word + " " + current);
+
+            return reversed.Trim();
+        }
+
+        public int DoCalculation(String expression)
+        {
+            var tokens = Regex.Split(expression, @"([\\+\\-])");
+
+            if (tokens.Length == 1)
+                return parseNum(expression);
+
+            var stack = new Stack<String>();
+            foreach (var token in tokens)
+            {
+                stack.Push(token);
+                if (stack.Count == 3)
+                {
+                    int and1 = parseNum(stack.ElementAt(2));
+                    String op = stack.ElementAt(1);
+                    int and2 = parseNum(stack.ElementAt(0));
+                    int result = 0;
+                    if (op == "+")
+                    {
+                        result = and1 + and2;
+                    }
+                    else if (op == "-")
+                    {
+                        result = and1 - and2;
+                    }
+                    stack.Clear();
+                    stack.Push("" + result);
+                }
+            }
+
+            return parseNum(stack.Peek());
+        }
+
+        public bool IsPalindrome(String palindrome)
+        {
+            char[] chars = palindrome.ToLower().ToCharArray();
+            for (int i = 0, j = (chars.Length - 1); i < (chars.Length / 2); i++, j--)
+            {
+                if (chars[i] != chars[j])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private int parseNum(String number)
+        {
+            try
+            {
+                return int.Parse(number);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Oops, can't parse " + number + " as a number!");
+                throw;
+            }
+        }
+    }
+}
---
+++ PracticalTest/PracticalTwo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalTest
+{
+    class PracticalTwo : IPracticalTwo
+    {
+        IEnumerable<IPerson> GetAncestors(IPerson person)
+        {
+            if (person == null) return new IPerson[0];
+;            return
+                (new[] {person.Mother, person.Father})
+                    .Concat(GetAncestors(person.Mother))
+                    .Concat(GetAncestors(person.Father))
+                    .Where(x => x != null);
+        }
+
+        public string[] NamesOfAllAncestors(IPerson person)
+        {
+            return GetAncestors(person).Select(p => p.Name).ToArray();
+        }
+
+        public string ModeHairColorOfAllAncestors(IPerson person)
+        {
+            return
+                GetAncestors(person)
+                    .GroupBy(p => p.HairColor)
+                    .OrderBy(g => g.Count())
+                    .Last()
+                    .Key;
+        }
+    }
+}
